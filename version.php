c1bd0aa94e3e4a5b09a92f709f6bfa28c6d35224
amand-0720 <amand@dynamicpixel.co.in>
2026-05-26 18:02:57 +0530

    Update version.php

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_haccgen_extender';
-$plugin->version   = 2026041002;
+$plugin->version   = 2026041003;
 $plugin->requires  = 2022112800;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.2';
